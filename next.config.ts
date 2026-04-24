430f7f78a943e678aeb68898e5b144af9520a89e
Harsh Singh <Harshroop100@gmail.com>
2026-04-24 19:11:53 +0000

    File changes

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'qigxixiekbdkeperulpk.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };

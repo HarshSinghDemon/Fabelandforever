cef0083ee218e33f4cad5f08c6ac96d5c2cd2170
Harsh Singh <Harshroop100@gmail.com>
2026-04-25 18:56:57 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,7 @@
-
 export const firebaseConfig = {
   "projectId": "studio-561863972-8b36c",
   "appId": "1:294566278146:web:c2be4dabc884270247a31b",
-  "apiKey": "AIzaSyCrbmELu--PLo_UHzZFXAXWIzc7URpPKGQ",
+  "apiKey": "AIzaSyBuvrAv0OQ0fKpNZ0_dM40FcVN4oOloyTE",
   "authDomain": "studio-561863972-8b36c.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "294566278146"

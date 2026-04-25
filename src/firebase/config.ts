db5fed81f263d16c1588fbf0a6e53ccf190f1c49
Harsh Singh <Harshroop100@gmail.com>
2026-04-25 13:25:01 +0000

    src/firebase/firestore/use-collection.tsx (94:33) @ useCollection.useEff

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,8 @@
+
 export const firebaseConfig = {
   "projectId": "studio-561863972-8b36c",
   "appId": "1:294566278146:web:c2be4dabc884270247a31b",
-  "apiKey": "AIzaSyBuvrAv0OQ0fKpNZ0_dM40FcVN4oOloyTE",
+  "apiKey": "AIzaSyCrbmELu--PLo_UHzZFXAXWIzc7URpPKGQ",
   "authDomain": "studio-561863972-8b36c.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "294566278146"
